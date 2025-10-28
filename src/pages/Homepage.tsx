683f099431df4a246039086aeba1cd69510966d9
Falk Kühnel <falk@kuehnel.eu>
2025-10-28 20:16:11 +0100

    feat: Just a Homepage template

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+export const Homepage = () => {
+    return (
+        <>
+            <h1>MakerSpace Bonn e.v.</h1>
+            <p>Willkommen bei unserem MakerSpace Inventar und Aufgaben System.</p>
+            <p>Hier können Sie Ihre Projekte verwalten und Ihre Aufgaben planen.</p>
+            <p>
+                <a href="/todos">Zu den Aufgaben</a>
+            </p>
+        </>
+    );
+};
