2a2e81d8e36d27f8ee4c8ad8954dfd2fc354f66a
Falk Kühnel <falk@kuehnel.eu>
2025-10-30 18:25:37 +0100

    chore: just a page for an item

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,39 +1,33 @@
-import {Elysia} from 'elysia'
-import {html} from "@elysiajs/html";
-import {staticPlugin} from '@elysiajs/static'
-import {discordAuthRoutes} from "./routes/discordAuthRoutes";
-import {inventorySearchRoutes} from "./routes/inventorySearchRoutes";
-import {projectRoutes} from "./routes/projectRoutes";
-import {migrateDB} from "./migrateDB";
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+import { staticPlugin } from "@elysiajs/static";
+import { discordAuthRoutes } from "./routes/discordAuthRoutes";
+import { inventorySearchRoutes } from "./routes/inventorySearchRoutes";
+import { projectRoutes } from "./routes/projectRoutes";
+import { migrateDB } from "./migrateDB";
+import React from "react";
 
 try {
-    await migrateDB();
+  await migrateDB();
 } catch (e) {
-    console.log(e)
+  console.log(e);
 }
 
-import {Home} from "../pages/Home";
-import {Page} from "../pages/Page";
+import { Homepage } from "../pages/Homepage";
+import { Home } from "../pages/Home";
+import { Page } from "../pages/Page";
 
 const app = new Elysia()
-    .use(html())
-    .use(staticPlugin())
-    .group('/auth', app => app.use(discordAuthRoutes))
-    .get('/login', () => `<a href="/auth/discord/login">Login with Discord</a>`)
-    .mount('/', inventorySearchRoutes)
-    .mount('/projects', projectRoutes)
-    .get('/test',  () => {
-        return (
-            <Page title="Zugland">
-                <Home fruit={['🍎', '🍊', '🍇', '🍉', '🍌']} />
-            </Page>
-        );
-    })
-    .listen(process.env.PORT || 3000)
+  .use(html())
+  .use(staticPlugin())
+  .group("/auth", (app) => app.use(discordAuthRoutes))
+  .get("/login", () => `<a href="/auth/discord/login">Login with Discord</a>`)
+  .mount("/search", inventorySearchRoutes)
+  .mount("/projects", projectRoutes)
+  .get("/", () => <Homepage />)
+    .get("/i/1250/", () => Bun.file(__dirname + "/../pages/1250.html"))
+  .listen(process.env.PORT || 3000);
 
 console.log(
-    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-)
-
-
-
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
