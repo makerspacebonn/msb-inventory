88db7e0a41aee3bd02427826fdec0d57891d3e3c
Falk Kühnel <falk@kuehnel.eu>
2025-11-02 23:56:07 +0100

    feat: item test layout with item 1250

--- src/pages/1250.tsx
+++ src/pages/1250.tsx
@@ -2,7 +2,7 @@ export const Item1250 = () => {
     return (
         <>
             <h1>1250</h1>
-            <p>Ein Teil vom MakerSpace</p>
+            <p>So ein Teil vom MakerSpace</p>
             <div>
                 <div><img class="item-picture" src="https://upload.wikimedia.org/wikipedia/commons/2/21/Pink_Fairy_Armadillo_%28Chlamyphorus_truncatus%29.jpg" /></div>
                 <div>ID: 1250</div>
