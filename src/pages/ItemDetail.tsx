2e29c4d6fbccb208d9734c8bf1946513a3bc898f
Falk Kühnel <falk@kuehnel.eu>
2025-11-12 17:43:34 +0100

    feat: showing images from db record

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -1,12 +1,13 @@
 import { ItemSelect } from "../drizzle/schema";
 
 export const ItemDetail = ({ item }: { item: ItemSelect }) => {
+    const imagePath = `/img/items/${item.imagePath}`;
     return (
         <>
             <h1>{item.id}</h1>
             <p safe>{item.name}</p>
             <div>
-                <div><img class="item-picture" src="https://upload.wikimedia.org/wikipedia/commons/2/21/Pink_Fairy_Armadillo_%28Chlamyphorus_truncatus%29.jpg" alt="Pink Fairy Armadillo" /></div>
+                <div><img class="item-picture" src={imagePath} alt={item.name} /></div>
                 <div safe>{item.description}</div>
                 <div>ID: {item.id}</div>
             </div>
