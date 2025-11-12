48a1da196b1a4add0fa51b01b471345848bf1cbc
Falk Kühnel <falk@kuehnel.eu>
2025-11-12 13:02:57 +0100

    feat: text adaptation

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,7 @@ export const Homepage = () => {
     return (
         <>
 
-            <h1>MakerSpace Bonn e.v.</h1>
+            <h1>Zeug beim MakerSpace Bonn e.v.</h1>
             <p>Willkommen bei unserem MakerSpace Zeug und Aufgaben System.</p>
             <p>Hier können Sie Ihre Projekte verwalten und Ihre Aufgaben planen.</p>
             <p>
