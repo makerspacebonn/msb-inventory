11f0113060267deb86d469ad6f525e3f670d7ddb
Falk Kühnel <falk@kuehnel.eu>
2025-11-12 18:51:38 +0100

    feat: location demo images

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -1,15 +1,26 @@
-import { ItemSelect } from "../drizzle/schema";
+import { ItemSelect, LocationSelect } from "../drizzle/schema";
+import { Location } from "./components/Location";
 
 export const ItemDetail = ({ item }: { item: ItemSelect }) => {
     const imagePath = `/img/items/${item.imagePath}`;
+    const location1: LocationSelect = { id: 1, name: "Falks Tasche", description: "This is an example location.", imagePath: "1.jpg", parentId: 2 , additionalInfo: "Additional information about Falks Tasche."};
+    const location2: LocationSelect = { id: 2, name: "Tisch am Monitor", description: "This is an example location.", imagePath: "2.jpg", parentId: 3, additionalInfo: "Additional information about Tisch am Monitor." };
+    const location3: LocationSelect = { id: 3, name: "Hauptraum", description: "This is an example location.", imagePath: "3.jpg", parentId: 4, additionalInfo: "Additional information about Hauptraum." };
+    const location4: LocationSelect = { id: 4, name: "MakerSpace", description: "This is an example location.", imagePath: "4.jpg", parentId: 4, additionalInfo: "Additional information about MakerSpace." };
+    const locationPath = [location1, location2, location3, location4];
     return (
         <>
-            <h1>{item.id}</h1>
-            <p safe>{item.name}</p>
+            <h1 safe>{item.name}</h1>
             <div>
-                <div><img class="item-picture" src={imagePath} alt={item.name} /></div>
+                <div  style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}><img class="item-picture" src={imagePath} alt={item.name} /></div>
                 <div safe>{item.description}</div>
                 <div>ID: {item.id}</div>
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap', width: '100%', justifyContent:'space-around'}}>
+                <Location location={location1} />
+                <Location location={location2} />
+                <Location location={location3} />
+                <Location location={location4} />
+                </div>
             </div>
         </>
     );
---
+++ src/pages/components/Location.tsx
@@ -0,0 +1,12 @@
+export const Location = ({ location }: { location: LocationSelect }) => {
+  const image = `/img/locations/${location.imagePath}`;
+  return (
+        <>
+          <div style={{  alignItems: 'center', position: 'relative', width: '50%', padding: '0.3rem'}}>
+            <img className="location-image" src={image} alt={location.name} style={{ borderRadius: '1rem', minWidth: '100%' }} />
+            <h1 style={{ textAlign: 'center',  fontSize: '1rem', position: 'absolute', bottom: '-5px', color: 'white', width: '100%' }}>{location.name}</h1>
+          </div>
+
+        </>
+    );
+};
