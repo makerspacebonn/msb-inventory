a61cf9088c728287b21a127b97bcf7a41406a72a
Falk Kühnel <falk@kuehnel.eu>
2025-11-18 04:19:01 +0100

    chore: cleaaned up Layout

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,46 +1,49 @@
 interface LayoutProps {
-    title: string,
-    children?: React.ReactNode,
+  title: string
+  children?: React.ReactNode
 }
-export const Layout = ({title, children }: LayoutProps) => {
-    return (
-        <>
-            <html>
-            <head>
-                <title safe>{ title } | Zeug MakerSpace Bonn e.V.</title>
-                <meta charset="utf-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <meta name="color-scheme" content="light dark"/>
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-                />
-                <link rel="stylesheet" href="/css/styles.css"/>
-            </head>
-            <body>
-            <header>
-                <section class="container">
-                    <nav>
-                        <ul>
-                            <li class="logo"><a href="#"><img src="/img/makerspace-bonn-signet.png" alt="MakerSpace Bonn e.V." /></a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="#"> MakerSpace Bonn e.V. </a></li>
-                        </ul>
-                        <ul>
-                            <li><a>Home</a></li>
-                            <li><a>Search</a></li>
-                        </ul>
-                    </nav>
-                </section>
-            </header>
-            <main class="container">
-                { children as 'safe' }
-            </main>
-
-            </body>
-            </html>
-
-        </>
-    );
-};
+export const Layout = ({ title, children }: LayoutProps) => {
+  return (
+    <html lang="de">
+      <head>
+        <title safe>{title} | Zeug MakerSpace Bonn e.V.</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="color-scheme" content="light dark" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+        />
+        <link rel="stylesheet" href="/css/styles.css" />
+      </head>
+      <body>
+        <header>
+          <section class="container">
+            <nav>
+              <ul>
+                <li class="logo">
+                  <a href="/">
+                    <img
+                      src="/img/makerspace-bonn-signet.png"
+                      alt="MakerSpace Bonn e.V."
+                    />
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <li>MakerSpace Bonn e.V.</li>
+              </ul>
+              <ul>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>Search</li>
+              </ul>
+            </nav>
+          </section>
+        </header>
+        <main class="container">{children as "safe"}</main>
+      </body>
+    </html>
+  )
+}
